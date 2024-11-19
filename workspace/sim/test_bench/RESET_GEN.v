33d0ff3159e78ac423377a461a2183ea7c72626b
gitomo0326 <90194271+gitomo0326@users.noreply.github.com>
2024-11-19 22:28:38 +0900

    change reset trigger edge

--- workspace/sim/test_bench/RESET_GEN.v
+++ workspace/sim/test_bench/RESET_GEN.v
@@ -7,9 +7,9 @@ reg p_rst;
 
 
 initial begin
-       p_rst = 0; #`RESET_TIMING;
-       p_rst = 1; #1;
-       p_rst = 0;
+    p_rst = 0; #`RESET_TIMING;
+    p_rst = 1; #1;
+    p_rst = 0;
 end
 
 assign oP_RST = p_rst;
--- workspace/src/10counter.v
+++ workspace/src/10counter.v
@@ -7,13 +7,13 @@ module counter10
 
 reg [3: 0] q;
 
-always @(posedge CLK or negedge RES) begin
-   if(RES ==1'b1)
-       q <= 4'd0;
-   else if(Q == 4'd9)
-       q <= 4'd0;
-   else
-       q <= Q + 4'd1;
+always @(posedge CLK or posedge RES) begin
+    if(RES)
+        q <= 4'd0;
+    else if(Q == 4'd9)
+        q <= 4'd0;
+    else
+        q <= Q + 4'd1;
 end
 
 assign Q = q;
