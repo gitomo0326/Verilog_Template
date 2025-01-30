774cd529bdda0fbc1008bc27245318363487b985
gitomo0326 <90194271+gitomo0326@users.noreply.github.com>
2025-01-30 22:51:11 +0900

    Make DE Genrerator

--- workspace/sim/test_bench/SYNC_GEN.v
+++ workspace/sim/test_bench/SYNC_GEN.v
@@ -10,7 +10,8 @@ module SYNC_GEN
     // Sync
     reg vs;
     reg hs;
-    reg de;
+    reg hde;
+    reg vde;
 
     // Sync Start Flag
     reg sync_on;
@@ -18,7 +19,6 @@ module SYNC_GEN
     initial begin
         vs <= 0;
         hs <= 0;
-        de <= 0;
         sync_on <= 0;
 
         @(posedge P_RST);
@@ -60,7 +60,46 @@ module SYNC_GEN
         end
     end
 
+    // Horizontal DE
+    always @(posedge P_CLK or posedge P_RST) begin
+        if(P_RST) begin
+            hde <= 1'b0;
+        end
+        else if(sync_on) begin
+            hde <= 1'b0;
+            repeat(`H_SYNC_WIDHT+`H_BACK_PORCH) @(posedge P_CLK);
+            hde <= 1'b1;
+            repeat(`H_DISP) @(posedge P_CLK);
+            hde <= 1'b0;
+            repeat(`H_TOTAL-(`H_SYNC_WIDHT+`H_BACK_PORCH+`H_DISP)-1) @(posedge P_CLK);
+        end
+        else begin
+            hde <= hde;
+        end
+    end
+
+    // Vertical DE
+    always @(posedge hs or posedge P_RST) begin
+        if(P_RST) begin
+            vde <= 1'b0;
+        end
+        else if(sync_on) begin
+            vde <= 1'b0;
+            repeat(`V_SYNC_WIDHT+`V_BACK_PORCH) @(posedge hs);
+            vde <= 1'b1;
+            repeat(`V_DISP) @(posedge hs);
+            vde <= 1'b0;
+            repeat(`V_TOTAL-(`V_SYNC_WIDHT+`V_BACK_PORCH+`V_DISP)-1) @(posedge hs);
+
+        end
+        else begin
+            vde <= vde;
+        end
+    end
+
+
     assign oVS = vs;
     assign oHS = hs;
+    assign oDE = hde & vde;
 
 endmodule
