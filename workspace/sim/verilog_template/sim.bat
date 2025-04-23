38f8850c5f2815e072805778f7e1d5c3f31b3a0c
gitomo0326 <90194271+gitomo0326@users.noreply.github.com>
2025-04-23 22:28:03 +0900

    #1 sim.batの置き場所を変更

--- workspace/sim/verilog_template/sim.bat
+++ workspace/sim/verilog_template/sim.bat
@@ -2,7 +2,7 @@
 
 set TEST_BENCH_TEMP="./test_bench_temp"
 set LOCAL_TEST_BENCH="./test_bench"
-set COMMON_TEST_BENCH="./common/test_bench"
+set COMMON_TEST_BENCH="../common/test_bench"
 
 if exist %TEST_BENCH_TEMP% (
     rmdir /s /q %TEST_BENCH_TEMP%
@@ -18,8 +18,8 @@ iverilog -I %TEST_BENCH_TEMP% ^
     -s SIM_TOP ^
     -c %TEST_BENCH_TEMP%/test_bench_rtl_file_list.cmd ^
     -c %TEST_BENCH_TEMP%/common_test_bench_rtl_file_list.cmd ^
-    -c ../src/rtl/rtl_lib_rtl_file_list.cmd ^
-    -c ../src/rtl/verilog_template_rtl_file_list.cmd
+    -c ../../src/rtl/rtl_lib_rtl_file_list.cmd ^
+    -c ../../src/rtl/verilog_template_rtl_file_list.cmd
 vvp a.out
 gtkwave sim.vcd
 del a.out
--- workspace/src/rtl/rtl_lib_rtl_file_list.cmd
+++ workspace/src/rtl/rtl_lib_rtl_file_list.cmd
@@ -1,3 +1,3 @@
-../src/rtl/RTL_LIB/DELAY/CYCLE_DELAY.v
-../src/rtl/RTL_LIB/REG/D_FF_V.v
-../src/rtl/RTL_LIB/REG/D_FF_R.v
+../../src/rtl/RTL_LIB/DELAY/CYCLE_DELAY.v
+../../src/rtl/RTL_LIB/REG/D_FF_V.v
+../../src/rtl/RTL_LIB/REG/D_FF_R.v
