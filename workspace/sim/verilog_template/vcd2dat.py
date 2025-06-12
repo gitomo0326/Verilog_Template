ad416c35bda40f9c14a704d0572e927644d00eea
gitomo0326 <90194271+gitomo0326@users.noreply.github.com>
2025-06-12 22:12:44 +0900

    #1 vcd2dat.py クラス化して整理

--- workspace/sim/verilog_template/vcd2dat.py
+++ workspace/sim/verilog_template/vcd2dat.py
@@ -1,4 +1,27 @@
+import argparse
 from vcdvcd import VCDVCD
+
+class SIM_VCD:
+    def __init__(self, vcd_file, module_name):
+        self.vcd = VCDVCD(vcd_file, store_tvs=True)
+        self.module_name = module_name
+        self.set_parameter()
+        self.set_video_stream_tv()
+
+    def set_parameter(self):
+        out_dat_wh   = self.vcd['SIM_TOP.OUT_DAT_WH'].tv
+        parallel_num = self.vcd['SIM_TOP.PARALLEL_NUM'].tv
+
+        self.out_dat_wh   = int(out_dat_wh[0][1], 2)
+        self.parallel_num = int(parallel_num[0][1], 2)
+
+    def set_video_stream_tv(self):
+        self.data_r       = self.vcd[f'SIM_TOP.m_{self.module_name}.oR[{self.out_dat_wh*self.parallel_num-1}:0]'].tv
+        self.data_g       = self.vcd[f'SIM_TOP.m_{self.module_name}.oG[{self.out_dat_wh*self.parallel_num-1}:0]'].tv
+        self.data_b       = self.vcd[f'SIM_TOP.m_{self.module_name}.oB[{self.out_dat_wh*self.parallel_num-1}:0]'].tv
+        self.de           = self.vcd[f'SIM_TOP.m_{self.module_name}.oDE'].tv
+        self.p_clk        = self.vcd['SIM_TOP.p_clk'].tv
+
 
 def find_value(tv, timestamp):
 
@@ -6,47 +29,51 @@ def find_value(tv, timestamp):
     value = dict_tv.get(timestamp, "invalid")
 
     return value        
-# File Read
-vcd = VCDVCD("sim.vcd", store_tvs=True)
 
-# Timestamp and value extraction
-data_r = vcd['SIM_TOP.m_VERILOG_TEMPLATE.oR[31:0]'].tv
-data_g = vcd['SIM_TOP.m_VERILOG_TEMPLATE.oG[31:0]'].tv
-data_b = vcd['SIM_TOP.m_VERILOG_TEMPLATE.oB[31:0]'].tv
-de     = vcd['SIM_TOP.m_VERILOG_TEMPLATE.oDE'].tv
-p_clk  = vcd['SIM_TOP.p_clk'].tv
+if __name__ == "__main__":
+    # Command Line Argument Parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("module_name")
+    args = parser.parse_args()
 
-p_clk_term = p_clk[2][0] - p_clk[0][0]
+    # File name and module name
+    vcd_file = "sim.vcd"
+    module_name = args.module_name
 
-p_clk_time = next((k for k, v in dict(p_clk).items() if v == "1"), None)
+    # Initialize VCD object
+    vcd = SIM_VCD(vcd_file, module_name)
 
-data_enable = 0
-de_flag = 0
-r = "invalid"
-g = "invalid"
-b = "invalid"
+    # Calculate P_CLK term and initial time
+    p_clk_term = vcd.p_clk[2][0] - vcd.p_clk[0][0]
+    p_clk_time = next((k for k, v in dict(vcd.p_clk).items() if v == "1"), None)
 
-output_file = "rgb_output.txt"
-with open(output_file, "w") as f:
+    data_enable = 0
+    de_flag = 0
+    r = "invalid"
+    g = "invalid"
+    b = "invalid"
 
-    count = 0
-    while p_clk_time <= p_clk[-1][0]:
+    output_file = "rgb_output.txt"
+    with open(output_file, "w") as f:
 
-        data_enable = find_value(de, p_clk_time)
+        count = 0
+        while p_clk_time <= vcd.p_clk[-1][0]:
 
-        de_flag = 1 if data_enable == '1' else (0 if data_enable == '0' else de_flag)
+            data_enable = find_value(vcd.de, p_clk_time)
 
-        if de_flag == 1:
-            r_temp = find_value(data_r, p_clk_time)
-            g_temp = find_value(data_g, p_clk_time)
-            b_temp = find_value(data_b, p_clk_time)
+            de_flag = 1 if data_enable == '1' else (0 if data_enable == '0' else de_flag)
 
-            r = r_temp if r_temp != "invalid" else r
-            g = g_temp if g_temp != "invalid" else g
-            b = b_temp if b_temp != "invalid" else b
+            if de_flag == 1:
+                r_temp = find_value(vcd.data_r, p_clk_time)
+                g_temp = find_value(vcd.data_g, p_clk_time)
+                b_temp = find_value(vcd.data_b, p_clk_time)
 
-            line = f"time:{p_clk_time}, de:{de_flag}, r:{hex(int(r, 2))}, g:{hex(int(g, 2))}, b:{hex(int(b, 2))}\n"
-            f.write(line)
-            count += 1
-            print(count)
-        p_clk_time += p_clk_term
+                r = r_temp if r_temp != "invalid" else r
+                g = g_temp if g_temp != "invalid" else g
+                b = b_temp if b_temp != "invalid" else b
+
+                line = f"time:{p_clk_time}, de:{de_flag}, r:{hex(int(r, 2))}, g:{hex(int(g, 2))}, b:{hex(int(b, 2))}\n"
+                f.write(line)
+                count += 1
+                print(count)
+            p_clk_time += p_clk_term
